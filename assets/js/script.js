d3267073cc60397ea9bef0412ed20e41bb46d419
TrevorTress <trevor.tress@outlook.com>
2022-03-23 14:34:35 -0400

    questions and buttons cycle

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,38 +1,55 @@
+
+// links to api's
 var ronUrl = "https://ron-swanson-quotes.herokuapp.com/v2/quotes"
 var movieUrl = 'https://movie-quote-api.herokuapp.com/v1/quote/'
 
+var swansonButtonEl = document.getElementById('swanson-btn')
+var movieButtonEl = document.getElementById('movie-btn')
+var displayedQuoteEl = document.getElementById('quote-display')
+
+// start-button starts the game
+var startButton = document.getElementById('start-btn')
+startButton.addEventListener('click', startGame)
+
+var nextButton = document.getElementById('next-btn')
+nextButton.addEventListener('click', advanceQuestion)
+
+// time function starts at whatever starting mins is and clears at 0
 const startingMins = 1
 let time = startingMins * 60
 var countdownEl = document.getElementById('countdown')
 
-
 function updateCountdown() {
     const minutes = Math.floor(time/60)
+
+    // annoying way of displaying seconds properly
     let seconds = time % 60
     seconds = seconds < 10 ? '0' + seconds : seconds
 
+    // if time is remaining, show it in the element and reduce it
     if (time >= 0) {
         countdownEl.innerHTML = `${minutes}:${seconds}`
         time --
+    // else clear the countdown    
     } else {
         clearInterval(updateCountdown)
     }
 }
 
+// main function that pulls apis and assigns values to quote and answer fields
 var getQuotes = function() {
 
     var quoteTypeRandom = Math.random()
     console.log(quoteTypeRandom)
-    setInterval(updateCountdown, 1000)
 
     if(quoteTypeRandom >= 0.5) { // ron quote displayed
 
         //fetch swanson api and update displayed quote
         fetch(ronUrl).then(function(response) {
             response.json().then(function(data) {
-                var displayedQuoteEl = document.getElementById('quote-display')
                 displayedQuoteEl.textContent = data[0]
-                var swansonButtonEl = document.getElementById('swanson-btn')
+
+                // experimental correct class add. need to be able to evaluate it. should probs switch to jquery
                 swansonButtonEl.classList.add('correct')
                 swansonButtonEl.addEventListener('click', function(event) {
                     console.log('swanson button worked')
@@ -43,7 +60,6 @@ var getQuotes = function() {
         //fetch movie api and update movie button
         fetch(movieUrl).then(function(response) {
             response.json().then(function(data) {
-                var movieButtonEl = document.getElementById('movie-btn')
                 movieButtonEl.textContent = data.role
             })
         })
@@ -52,12 +68,11 @@ var getQuotes = function() {
 
         // fetch movie api and update displayed quote AND movie button
         fetch(movieUrl).then(function(response) {
-            response.json().then(function(data) {
-                var displayedQuoteEl = document.getElementById('quote-display')
+            response.json().then(function(data) {               
                 displayedQuoteEl.textContent = data.quote
-
-                var movieButtonEl = document.getElementById('movie-btn')
                 movieButtonEl.textContent = data.role
+
+                // experimental correct class add. need to be able to evaluate it. should probs switch to jquery
                 movieButtonEl.classList.add('correct')
                 movieButtonEl.addEventListener('click', function(event) {
                     console.log('movie button worked')
@@ -67,7 +82,36 @@ var getQuotes = function() {
     }
 }
 
-getQuotes()
+function startGame() {
+    //start the countdown
+    setInterval(updateCountdown, 1000)
+
+    // hide the start button, show the next button
+    $(startButton).toggle('hide');
+    $(nextButton).toggle('hide');
+
+    // display the answer boxes
+    $(swansonButtonEl).toggle('hide')
+    $(movieButtonEl).toggle('hide')
+
+    // pull the apis and populate info
+    getQuotes()  
+}
+
+function selectAnswer(event) {    
+        var selectedButton = event.target
+        var correct = selectedButton.dataset.correct
+}
+
+// advances 'question' (resets buttons and pulls new quote)
+function advanceQuestion() {
+    // reset correct status
+    $(swansonButtonEl).removeClass('correct');
+    $(movieButtonEl).removeClass('correct');
+
+    // pull the apis and populate info
+    getQuotes()
+}
 
 ////////////////////////////////////////////////////////////////
 
